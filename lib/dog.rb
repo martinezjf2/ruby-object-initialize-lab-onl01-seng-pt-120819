d9f84a4ffa54654e0676b341e045658d5f47f6bb
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-09 20:35:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,15 @@
 class Dog
+  def initialize(breed)
+    @breed = breed
+  end
 
+  def breed=(breed)
+    @breed = breed
+  end
 
-
-
-
-
-
-  
+  def breed
+    @breed
+  end
 end
+
+fido = Dog.new("Pug")
